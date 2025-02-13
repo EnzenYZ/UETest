e206b1f7e784197f7fc8ca5cf7bed432f60b3314
EnzenYZ <hozhen99@gmail.com>
2025-02-14 03:30:42 +0800

    13/2/2025
    Quest system

---
+++ YZ_Test/Source/YZ_Test/QuestDataTypes.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "QuestDataTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FQuestObjective
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "QuestObjective")
+	FText ObjectiveName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "QuestObjective")
+	FText ObjectiveDescription;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "QuestObjective")
+	FString ObjectiveID;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "QuestObjective")
+	int32 Quantity = 0;
+	
+};
+
+USTRUCT(BlueprintType)
+struct FQuestList : public FTableRowBase
+{
+
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "QuestData")
+	FText QuestName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "QuestData")
+	FText QuestDescription;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "QuestData")
+	TArray<FQuestObjective> QuestObjective;
+	
+};
+
+
